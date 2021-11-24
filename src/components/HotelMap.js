63686cd64a1757bfae793cfa725ddbc63373fc81
daoraCode <severin.mboukou@gmail.com>
2021-11-24 09:44:21 +0100

    updated HotelMap.js

--- src/components/HotelMap.js
+++ src/components/HotelMap.js
@@ -1,9 +1,26 @@
-import React from 'react';
+import GoogleMapReact from 'google-map-react';
+// import styled from 'styled-components';
+import { useEffect } from 'react';
 
 const HotelMap = () => {
+
+    // (hook) équivalent du composantDidMount
+    useEffect(() => {
+        navigator.geolocation.getCurrentPosition(
+            location => {
+                console.log(location.coords);
+            }
+        )
+    }, [])
+
     return (
-        <div>
-            <h1>Hotel Map</h1>
+        <div style={{ height: '100vh', width: '100%' }}>
+            <GoogleMapReact
+                bootstrapURLKeys={{ key: "" }} 
+                defaultCenter={{ lat: 48.8666883, lng: 2.4174073 }}
+                defaultZoom={25}
+            >
+            </GoogleMapReact>
         </div>
     );
 };
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Nav from '../components/Nav';
+import HotelMap from '../components/HotelMap';
 
-import CityCard from '../components/CityCard';
+// import CityCard from '../components/CityCard';
 
 const Home = () => {
     return (
         <div>
             <Nav />
+            <HotelMap />
             <h1>Home</h1>
         </div>
     );
