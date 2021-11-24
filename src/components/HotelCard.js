9d62b1d7f837d463d8157ff8565ca353b86b8c89
Vincent <vins@Pimentas-MacBook-Air.local>
2021-11-24 11:45:00 +0100

    display img dynamique

--- src/components/HotelCard.js
+++ src/components/HotelCard.js
@@ -1,7 +1,7 @@
 import {React , useState, useEffect } from 'react';
 import styled from 'styled-components'
 import { useParams } from 'react-router-dom';
-
+import arrayImage from './Img';
 
 
 const Hotel = styled.div`
@@ -20,11 +20,14 @@ const HotelContainer = styled.div`
      gap: 20px ;
 
 ` 
-
 const HotelCard = props => {
 
+    
+
+  
       const [hotels , setHotels] = useState(null)
       const { city } = useParams()
+      console.log(city);
        
     // console.log(`form hotelcards : ${city}`);
 
@@ -35,17 +38,30 @@ const HotelCard = props => {
       .then(response => response.json())
       .then(data => setHotels(data))
   }, [])
-
+   
     if(!hotels){
         return (
         <p>Loading Data , please wait </p>
         )
+        
    }
+   console.log(hotels.results);
     return (
         <HotelContainer>
-            {hotels.results.map(hotel => {return(
-                        <Hotel key={Hotel.name}>
-                            <img src={"https://trippy-konexio.herokuapp.com/img/hotels/197200_29.jpg"} alt={hotel.name}/>
+            {hotels.results.map(hotel => {
+                var src = hotel.pictures.find(picture => arrayImage.includes(picture)) 
+                if(src){
+                    src = 'https://trippy-konexio.herokuapp.com'+src
+                }
+                else{src = 'https://media.istockphoto.com/photos/downtown-cleveland-hotel-entrance-and-waiting-taxi-cab-picture-id472899538?b=1&k=20&m=472899538&s=170667a&w=0&h=oGDM26vWKgcKA3ARp2da-H4St2dMEhJg23TTBeJgPDE=' }
+                
+                console.log(src);
+                return(
+                        <Hotel key={hotel.name}>
+                            <img src={src}
+     
+                         alt={hotel.name}/>
+
                             <p>{hotel.name}</p>
                             <p>{hotel.price} Euro</p>
                             <p>{hotel.stars} Stars</p>
