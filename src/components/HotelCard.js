ddc8fa59be96ddf6e958be251a645fa169e67f0c
Alin Manole <alin.manole@yahoo.com>
2021-11-24 12:25:03 +0100

    stars update

--- src/components/HotelCard.js
+++ src/components/HotelCard.js
@@ -2,6 +2,7 @@ import { React, useState, useEffect } from 'react';
 import styled from 'styled-components'
 import { useParams } from 'react-router-dom';
 import arrayImage from './Img';
+import ReactStars from "react-rating-stars-component";
 
 const Image = styled.img`
     background-image: url("src");
@@ -48,29 +49,33 @@ const HotelCard = props => {
         return (
             <p>Loading Data , please wait </p>
         )
-        
-   }
-   console.log(hotels.results);
+
+    }
+    console.log(hotels.results);
     return (
         <HotelContainer>
             {hotels.results.map(hotel => {
-                var src = hotel.pictures.find(picture => arrayImage.includes(picture)) 
-                if(src){
-                    src = 'https://trippy-konexio.herokuapp.com'+src
+                var src = hotel.pictures.find(picture => arrayImage.includes(picture))
+                if (src) {
+                    src = 'https://trippy-konexio.herokuapp.com' + src
                 }
-                else{src = 'https://media.istockphoto.com/photos/downtown-cleveland-hotel-entrance-and-waiting-taxi-cab-picture-id472899538?b=1&k=20&m=472899538&s=170667a&w=0&h=oGDM26vWKgcKA3ARp2da-H4St2dMEhJg23TTBeJgPDE=' }
-                
-                console.log(src);
-                return(
-                        <Hotel key={hotel.name}>
-                            <Image src={src}
-     
-                         alt={hotel.name}/>
+                else { src = 'https://media.istockphoto.com/photos/downtown-cleveland-hotel-entrance-and-waiting-taxi-cab-picture-id472899538?b=1&k=20&m=472899538&s=170667a&w=0&h=oGDM26vWKgcKA3ARp2da-H4St2dMEhJg23TTBeJgPDE=' }
 
-                            <p>{hotel.name}</p>
-                            <p>{hotel.price} Euro</p>
-                            <p>{hotel.stars} Stars</p>
-                       
+                console.log(src);
+                return (
+                    <Hotel key={hotel.name}>
+                        <Image src={src}
+
+                            alt={hotel.name} />
+
+                        <p>{hotel.name}</p>
+                        <p>{hotel.price} Euro</p>
+                        <ReactStars
+                            count={hotel.stars}
+                            size={24}
+                            color="#ffd700"
+                        />
+
                     </Hotel>)
             })}
 
