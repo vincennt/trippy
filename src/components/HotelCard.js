14d0193a46e2912c4d42a373c2ada3598eae3b78
Vincent <vins@Pimentas-MacBook-Air.local>
2021-11-25 10:08:22 +0100

    ee

--- src/components/HotelCard.js
+++ src/components/HotelCard.js
@@ -1,9 +1,8 @@
-import { React, useState, useEffect, Link } from 'react';
+import { React, useState, useEffect } from 'react';
 import styled from 'styled-components'
 import { useParams } from 'react-router-dom';
-
+import HotelMap from '../components/HotelMap';
 import arrayImage from './Img';
-import ReactStars from "react-rating-stars-component";
 
 
 const Image = styled.img`
@@ -22,6 +21,7 @@ flex-direction: row ;
 const Hotel = styled.div`
     width: 320px ;
     background-image: url("src");
+    // background: linear-gradient(to bottom, #fff 50%, #e0e0e0 100%);
     border-radius: 10px;
     border: 2px solid;
     font-weight: bold;
@@ -39,13 +39,14 @@ const HotelContainer = styled.div`
 `
 
 const HotelCard = props => {
+
     const [hotels, setHotels] = useState(null)
     const { city } = useParams()
-    console.log(`form hotelcards : ${city}`);
 
-    const handleAddFav = () =>{
-    console.log('add');
-}
+
+    // console.log(`form hotelcards : ${city}`);
+
+
 
     useEffect(() => {
         fetch(`https://trippy-konexio.herokuapp.com/api/hotels/city/${city}?page=${props.pageNumber}`)
@@ -61,35 +62,30 @@ const HotelCard = props => {
     }
     console.log(hotels.results);
     return (
-        <HotelContainer>
-            {hotels.results.map(hotel => {
-                var src = hotel.pictures.find(picture => arrayImage.includes(picture))
-                if (src) {
-                    src = 'https://trippy-konexio.herokuapp.com' + src
-                }
-                else { src = 'https://media.istockphoto.com/photos/downtown-cleveland-hotel-entrance-and-waiting-taxi-cab-picture-id472899538?b=1&k=20&m=472899538&s=170667a&w=0&h=oGDM26vWKgcKA3ARp2da-H4St2dMEhJg23TTBeJgPDE=' }
+        <Map>
+            <HotelContainer>
+                {hotels.results.map(hotel => {
+                    var src = hotel.pictures.find(picture => arrayImage.includes(picture))
+                    if (src) {
+                        src = 'https://trippy-konexio.herokuapp.com' + src
+                    }
+                    else { src = 'https://media.istockphoto.com/photos/downtown-cleveland-hotel-entrance-and-waiting-taxi-cab-picture-id472899538?b=1&k=20&m=472899538&s=170667a&w=0&h=oGDM26vWKgcKA3ARp2da-H4St2dMEhJg23TTBeJgPDE=' }
 
-                console.log(src);
-                return (
-                    <Link key={hotel._id} to={`/hotels/${city}/${hotel._id}`}>
-                    <Hotel key={hotel.name}>
-                        <Image src={src}
+                    console.log(src);
+                    return (
+                        <Hotel key={hotel.name}>
+                            <Image src={src}
 
-                            alt={hotel.name} />
+                                alt={hotel.name} />
 
-                        <p>{hotel.name}</p>
-                        <p>{hotel.price} Euro</p>
-                        <ReactStars
-                            count={hotel.stars}
-                            size={24}
-                            color="#ffd700"
-                        />
-                        <button onClick={handleAddFav}>add fav</button>
-                    </Hotel>
-                    </Link>)
-            })}
-        <Map/>
-        </HotelContainer>
+                            <p>{hotel.name}</p>
+                            <p>{hotel.price}€</p>
+                            <p>{hotel.stars} Stars</p>
+                        </Hotel>)
+                })}
+            </HotelContainer>
+            <HotelMap hotels={hotels} />
+        </Map>
     );
 };
 
