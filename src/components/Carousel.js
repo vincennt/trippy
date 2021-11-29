26d477b7ad2e96ab5704c31248ec96a87b92a97e
Alin Manole <alin.manole@yahoo.com>
2021-11-29 11:31:07 +0100

    finish carousel

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -2,11 +2,12 @@
 import { React } from 'react';
 import { CarouselProvider, Slider, Slide, ButtonBack, ButtonNext, Image } from 'pure-react-carousel';
 import 'pure-react-carousel/dist/react-carousel.es.css';
+import Img from './Img'
 
 
 const Carousel = (props) => {
 
-
+    
     //appel API des infos de la home page
 
     // console.log(props)
@@ -22,37 +23,37 @@ const Carousel = (props) => {
             <ButtonNext>Next</ButtonNext>
             <Slider>
                 <Slide index={0}>
-                    <Image src="https://picsum.photos/1600/900" alt="zero foto" />
+                    <Image src="https://trippy-konexio.herokuapp.com/img/hotels/10066892_18.jpg" alt="zero foto" />
                 </Slide>
                 <Slide index={1}>
-                    <Image src="https://picsum.photos/1600/900/" alt="first foto" />
+                    <Image src="https://trippy-konexio.herokuapp.com/img/hotels/10066892_19.jpg" alt="first foto" />
                 </Slide>
                 <Slide index={2}>
-                    <Image src="https://picsum.photos/1600/900/" alt="second foto" />
+                    <Image src="https://trippy-konexio.herokuapp.com/img/hotels/10066892_24.jpg" alt="second foto" />
                 </Slide>
                 <Slide index={3}>
-                    <Image src="https://picsum.photos/1600/900/" alt="third foto" />
+                    <Image src="https://trippy-konexio.herokuapp.com/img/hotels/10066892_25.jpg" alt="third foto" />
                 </Slide>
                 <Slide index={4}>
-                    <Image src="https://picsum.photos/1600/900/" alt="four foto" />
+                    <Image src="https://trippy-konexio.herokuapp.com/img/hotels/10319203_25.jpg" alt="four foto" />
                 </Slide>
                 <Slide index={5}>
-                    <Image src="https://picsum.photos/1600/900/" alt="five foto" />
+                    <Image src="https://trippy-konexio.herokuapp.com/img/hotels/10319203_30.jpg" alt="five foto" />
                 </Slide>
                 <Slide index={6}>
-                    <Image src="https://picsum.photos/1600/900" alt="six foto" />
+                    <Image src="https://trippy-konexio.herokuapp.com/img/hotels/10541730_48.jpg" alt="six foto" />
                 </Slide>
                 <Slide index={7}>
-                    <Image src="https://picsum.photos/1600/900" alt="seven foto" />
+                    <Image src="https://trippy-konexio.herokuapp.com/img/hotels/12533513_10.jpg" alt="seven foto" />
                 </Slide>
                 <Slide index={8}>
-                    <Image src="https://picsum.photos/1600/900" alt="eight foto" />
+                    <Image src="https://trippy-konexio.herokuapp.com/img/hotels/11621939_32.jpg" alt="eight foto" />
                 </Slide>
                 <Slide index={9}>
-                    <Image src="https://picsum.photos/1600/900" alt="nine foto" />
+                    <Image src="https://trippy-konexio.herokuapp.com/img/hotels/112064_45.jpg" alt="nine foto" />
                 </Slide>
                 <Slide index={10}>
-                    <Image src="https://picsum.photos/1600/900" alt="ten foto" />
+                    <Image src="https://trippy-konexio.herokuapp.com/img/hotels/11854022_21.jpg" alt="ten foto" />
                 </Slide>
             </Slider>
         </CarouselProvider>
