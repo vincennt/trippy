02fc21441bc0bc3e6bb86af115a24de967b83ea1
Vincent <vins@Pimentas-MacBook-Air.local>
2021-11-29 16:14:00 +0100

    fav whitout button

--- src/components/Fav.js
+++ src/components/Fav.js
@@ -2,11 +2,13 @@ import React from 'react'
 import { useEffect,useState } from 'react'
 import styled from 'styled-components'
 import { Link } from 'react-router-dom'
-
-const Hotel = styled.div`
-width : 500px;
-height 500px;
+import arrayImage from './Img'
+const P = styled.p`
+font-size : 18px;
+margin-left:2px,
 `
+const Dive = styled.div 
+`margin-left : 2px`
 
 const Image = styled.img`co
     background-image: url("src");
@@ -16,81 +18,124 @@ const Image = styled.img`co
 `
 
 
+
+const Hotel = styled.div`
+display :flex;
+flex-direction : column;
+justify-content : center;
+    width: 300px ;
+    background-image: url("src");
+    // background: linear-gradient(to bottom, #fff 50%, #e0e0e0 100%);
+    border-radius: 10px;
+    font-weight: bold;
+    margin: 0 1em;
+    background-color: #dbdbdb ;
+    padding: 0 0 10px  0;
+    margin: 20px;
+    height: 400px;
+`
+const HotelContainer = styled.div`
+
+display:flex ; 
+flex-direction: column;
+align-items : center;
+justify-content : center ; 
+
+@media (min-width : 725px){
+display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    margin: 15px;
+    flex-direction: column ;
+    align-items: center;
+    gap: 20px ;
+}
+`
+const Button = styled.button`
+box-shadow: 0px 1px 0px 0px #fff6af;
+	background:linear-gradient(to bottom, #ffec64 5%, #ffab23 100%);
+	background-color:#ffec64;
+	border-radius:6px;
+	border:1px solid #ffaa22;
+	display:inline-block;
+	cursor:pointer;
+	color:#333333;
+	font-family:Arial;
+	font-size:15px;
+	font-weight:bold;
+	padding:6px 24px;
+	text-decoration:none;
+	text-shadow:0px 1px 0px #ffee66;
+`
+
 export default function Fav() {
 
-// localStorage container
-const [favorites, setFavorites] = useState(localStorage.getItem("ID"))
+    
+     const handleAddStorage=(id)=>{
+        const favorites = localStorage.getItem("ID")
+        if (!favorites){
+            localStorage.setItem("ID", JSON.stringify([id]))
+        }
+        else{
+            let array = JSON.parse(favorites)
+            array = [...array , id] 
+            console.log(array);
+            localStorage.setItem("ID", JSON.stringify(array))
+            
+        }
+        
+    }
+
 
 //api container
-const [hotel , setHotel] = useState(null)
-// console.log(favorites , typeof favorites , "favorites");
-// console.log(hotel , typeof hotel, "hotel");
-   
+const [hotel , setHotel] = useState([])
 
-
-
-  
-
-    //   useEffect(() => {
-    //       if(favorites.lenght>0){
-    //     hotel.forEach(ide => {
-    //         getHotel(ide)
-    //     }) }
-    // }, [])
-
-    
 
       useEffect(() =>{
-        fetch(`https://trippy-konexio.herokuapp.com/api/hotels/`)
-        .then(res => res.json())
-        .then(data => setHotel([data]))
-    },[])
+        const favoritesIds = JSON.parse(localStorage.getItem("ID"))
+        const promiseArray = favoritesIds.map(id => {
+        return fetch(`https://trippy-konexio.herokuapp.com/api/hotels/${id}`)
+    })  
 
-  
+    Promise.all(promiseArray)
+      .then(responses => Promise.all(responses.map(response => response.json())))
+      .then(response => {
+        const formattedHotels = response.map(r => r.result)
+        setHotel(formattedHotels)
+      })
+  }, [])
 
-    //  useEffect(() =>{
-    //   favorites.forEach(hotelId => {
-    //   getHotel(hotelId)       
-    //  })})
-
-    //  useEffect( () => {
-
-    //      favorites.forEach( e => {
-    //      getHotel(e)
-    //      }) 
-    //  },[])
-
-    
-
-const handleClick =()=>{
-    console.log('hotel : ',hotel);
-    console.log("fav :",favorites);
-    // console.log('id : ',id);
-}
-
-if(!hotel){
-    return (<>
-    <p>loading your favorite</p>
-        <button onClick={()=>handleClick()}>log </button>
-        </>)
-}
-
+  if(hotel.length === 0){
+      return (<p>Loading</p>)
+  }
     return (
-        <div>
-            <button onClick={()=>handleClick()}>log </button>
-            
-            {hotel.map(fav =>(
-                <Link key={fav._id} to={`/hotels/${hotel._id}`}>
-                        <Hotel key={fav.name}>
+        <div>          
+            <HotelContainer>
+                {hotel.map(hotel => {
+                    var src = hotel.pictures.find(picture => arrayImage.includes(picture))
+                    if (src) {
+                        src = 'https://trippy-konexio.herokuapp.com' + src
+                    }
+                    else { src = 'https://media.istockphoto.com/photos/downtown-cleveland-hotel-entrance-and-waiting-taxi-cab-picture-id472899538?b=1&k=20&m=472899538&s=170667a&w=0&h=oGDM26vWKgcKA3ARp2da-H4St2dMEhJg23TTBeJgPDE=' }
+
+                    
+                    return (
+                         
+                        <Hotel key={hotel.name}>
+                         <Link key={hotel._id} to={`/hotels/${hotel._id}`}>                                                        
                             <Image
-                            src="https://media.istockphoto.com/photos/downtown-cleveland-hotel-entrance-and-waiting-taxi-cab-picture-id472899538?b=1&k=20&m=472899538&s=170667a&w=0&h=oGDM26vWKgcKA3ARp2da-H4St2dMEhJg23TTBeJgPDE="
-                            alt={fav.name} />
-                            <p>{fav.name}</p>
-                            <p>{fav.price}€</p>
-                            <p>{fav.stars} Stars</p>
-                        </Hotel>
-                        </Link>
-            ))}
+                            src={src} 
+                            alt={hotel.name} />
+                            </Link>
+                            <Dive>
+                                <P>{hotel.name}</P>
+                                <P>{hotel.price}€</P>
+                                <P>{hotel.stars} Stars</P>
+                            </Dive>                            
+                        <Button onClick={()=>handleAddStorage(hotel._id)}>Add Fav</Button>
+                        </Hotel>)
+                })}
+            </HotelContainer>  
+            ))
         </div>
     )
 
