033f99a7decb19520752ad6b2cae47c9904cf686
Alin Manole <alin.manole@yahoo.com>
2021-11-30 11:29:53 +0100

    update

--- src/components/HotelInfo.js
+++ src/components/HotelInfo.js
@@ -48,30 +48,8 @@ const P = styled.p`
     @import url('https://fonts.googleapis.com/css2?family=PT+Sans&display=swap');
     font-family: 'PT Sans', sans-serif; 
 `
-const BouttonContainer = styled.div`
-display : flex ; 
-margin: 10px;
-margin-left: 35px;
-gap: 4px;
-`
 
-const ButtonPage = styled.button`
 
-	box-shadow:inset 0px 1px 0px 0px #ffffff;
-	background:linear-gradient(to bottom, #ffffff 5%, #f6f6f6 100%);
-	background-color:#ffffff;
-	border-radius:6px;
-	border:2px solid #dcdcdc;
-	display:inline-block;
-	cursor:pointer;
-	color:#666666;
-	font-family:Arial;
-	font-size:15px;
-	font-weight:bold;
-	padding:6px 24px;
-	text-decoration:none;
-	text-shadow:0px 1px 0px #ffffff;
-    `
 
 const HotelInfo = () => {
     const { id } = useParams()
